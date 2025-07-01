7ab3b29c8c5885d3b75f8a7981b29867f12698dd
maryamalihasanebrahim <maryamalihasanebrahim@gmail.com>
2025-07-01 09:12:05 +0300

    submitted cards and their content plus text 'animation'

--- game-script.js
+++ game-script.js
@@ -1,5 +1,38 @@
-let cards = document.querySelector(".cards")
-let card_number = 0
+let cards = document.querySelectorAll('.cards')
+let fruit = document.querySelectorAll('.fruit')
+let start = document.querySelector('.message')
+let popup = document.querySelector('.popup')
+let flag = false
 
-card_number = Math.random()
-console.log(card_number)
+
+let startGame = () => {
+  flag = true
+  for (let i = 0; i < cards.length; i++) {
+    console.log('success')
+    if (flag) {
+      fruit[i].style.opacity = '1'
+      setTimeout(() => {
+        console.log('this works')
+        fruit[i].style.opacity = '0'
+      }, 2000)
+    }
+  }
+  setTimeout(() => {
+    popup.style.opacity = '1'
+    popup.innerText = 'pick your first card'
+  }, 2000)
+} 
+
+setTimeout(() => {
+  popup.innerText = 'ready?'
+}, 1000)
+setTimeout(() => {
+  popup.innerText = 'set.'
+}, 2000)
+setTimeout(() => {
+  popup.innerText = 'go!'
+  startGame()
+}, 3000)
+
+
+
