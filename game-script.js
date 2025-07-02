59a6b5fc2cb3b4b043f15d19036afe2916680c8f
maryamalihasanebrahim <maryamalihasanebrahim@gmail.com>
2025-07-02 15:56:02 +0300

    play again

--- game-script.js
+++ game-script.js
@@ -1,3 +1,6 @@
+// whats left
+// timer, larger grid for levels, shuffle
+
 // VARIABLES
 let cards = document.querySelectorAll('.cards')
 let fruit = document.querySelectorAll('.fruit')
@@ -15,7 +18,9 @@ let heartBeat = false
 let background = document.querySelectorAll('button')
 let firstIndex = 0
 let secondIndex = 0
-let enable = false
+let cardsFinished = false
+let board = document.querySelector('.threeBythree')
+playAgain = document.createElement('button')
 
 // FUNCTIONS
 updateScore = () => {
@@ -23,45 +28,95 @@ updateScore = () => {
   bestScore++
   scoreDisplay.innerText = 'new: ' + currentScore
   bestDisplay.innerText = 'best: ' + bestScore
+  if (currentScore >= 6) {
+    currentScore = 0
+    heartBeat = false
+    board.style.opacity = '0'
+    popup.innerText = 'you win!'
+    popup.appendChild(playAgain).innerText = 'play again'
+    playAgain.style.visibility = 'visible'
+    hoverOverPlayAgain()
+  }
+  // return
 }
 
 let halfFlip = (firstIndex, secondIndex) => {
-  console.log('enters function')
-  console.log("when no equal first card is" + firstCard)
-  console.log("when no equal first card is" + secondCard.id)
   if (firstCard !== secondCard.id) {
-    fruit[firstIndex].style.opacity = '1'
-    fruit[secondIndex].style.opacity = '1'
-    background[firstIndex].style.backgroundColor = 'white'
-    background[secondIndex].style.backgroundColor = 'white'
+    setTimeout(() => {
+      // console.log('two cards arent the same ')
+      // console.log('index 1 is ' + firstIndex + 'index 2 is ' + secondIndex)
+      fruit[firstIndex].style.opacity = '0'
+      fruit[secondIndex].style.opacity = '0'
+      background[firstIndex].style.backgroundColor = 'rgb(255, 228, 228)'
+      background[secondIndex].style.backgroundColor = 'rgb(255, 228, 228)'
+    }, 1000)
+    popup.innerText = 'try again'
+    popup.style.opacity = '1'
+    setTimeout(() => {
+      popup.innerText = ''
+    }, 2000)
   }
-  setTimeout(() => {
-    fruit[firstIndex].style.opacity = '0'
-    fruit[secondIndex].style.opacity = '0'
-    background[firstIndex].style.backgroundColor = 'rgb(255, 228, 228)'
-    background[secondIndex].style.backgroundColor = 'rgb(255, 228, 228)'
-  }, 2000)
-  console.log('try again')
+  return
 }
 
-console.log([fruit])
+let hoverOverPlayAgain = () => {
+  // console.log('firstCard', firstCard)
+  // console.log('second card', secondCard)
+  playAgain.addEventListener('mouseover', () => {
+    playAgain.style.opacity = '0.5'
+  })
+  playAgain.addEventListener('mouseout', () => {
+    playAgain.style.opacity = '1'
+  })
+  playAgain.addEventListener('click', () => {
+    playAgain.style.display = 'none'
+    board.style.opacity = '1'
+    for (let i = 0; i < cards.length; i++) {
+      console.log('enters remover')
+
+      background[i].style.backgroundColor = 'rgb(255, 228, 228)'
+      cards[i].removeEventListener('click', cardGame)
+    }
+    setTimeout(() => {
+      popup.innerText = 'ready?'
+    }, 1000)
+    setTimeout(() => {
+      popup.innerText = 'set.'
+    }, 2000)
+    setTimeout(() => {
+      popup.innerText = 'go!'
+      console.log(
+        'In hoveroverplayagain, is there a card? ' + firstCard + secondCard
+      )
+      startGame()
+    }, 3000)
+  })
+}
+
 let flippingAction = (firstIndex, secondIndex) => {
   if (firstCard === secondCard.id) {
+    // console.log('two cards are the same')
     fruit[firstIndex].style.opacity = '1'
     fruit[secondIndex].style.opacity = '1'
     background[firstIndex].style.backgroundColor = 'white'
     background[secondIndex].style.backgroundColor = 'white'
     updateScore()
+    console.log('matched! resetting card choices')
+    firstCard = ''
+    secondCard = ''
+    firstCardElement = ''
     return
-  }
-
-  else {
+  } else {
     halfFlip(firstIndex, secondIndex)
   }
 }
 
-
 let startGame = () => {
+  firstCard = ''
+  firstCardElement = ''
+  secondCard = ''
+
+  console.log('In startgame, is there a card? ' + firstCard + secondCard)
   flag = true
   for (let i = 0; i < cards.length; i++) {
     if (flag) {
@@ -93,38 +148,42 @@ setTimeout(() => {
 
 heartOfTheGame = () => {
   for (let i = 0; i < cards.length && heartBeat; i++) {
-    cards[i].addEventListener('click', () => {
-      // to prevent a card that has been clicked to be clicked again and provide the output of a pair
-      if (cards[i] === firstCardElement) {
-        return
-      }
-      console.log('first card: ' + firstCard)
-      console.log('second card:' + secondCard)
-      if (!firstCard) {
-        firstCard = cards[i].id
-        firstCardElement = cards[i]
-        console.log('first card flipped')
-        firstIndex = i
-        return
-      }
-      if (!secondCard) {
-        secondCard = cards[i]
-        console.log('second card flipped')
-        secondIndex = i
-        console.log(firstIndex + ' and ' + secondIndex)
-        flippingAction(firstIndex, secondIndex)
-        firstCard = ''
-        secondCard = ''
-        console.log('first card: ' + firstCard)
-        console.log('second card:' + secondCard)
-        firstCardElement = ''
-        return
-      }
-    })
+    console.log('Adding event listeners..')
+    cards[i].addEventListener('click', () => cardGame(i))
   }
 }
 
+cardGame = (i) => {
+  popup.innerText = ''
+  // to prevent a card that has been clicked to be clicked again and provide the output of a pair
+  if (cards[i] === firstCardElement) {
+    return
+  }
 
+  if (!firstCard) {
+    firstCard = cards[i].id
+    firstCardElement = cards[i]
+    fruit[i].style.opacity = '1'
+    background[i].style.backgroundColor = 'white'
+    console.log('first card flipped')
+    // console.log(i)
+    firstIndex = i
+    return
+  }
+  if (!secondCard) {
+    console.log('first card already picked! and is ' + firstCard)
+    secondCard = cards[i]
+    console.log('second card flipped')
+    // console.log(i)
+    fruit[i].style.opacity = '1'
+    background[i].style.backgroundColor = 'white'
+    secondIndex = i
+    flippingAction(firstIndex, secondIndex)
+    firstCard = ''
+    secondCard = ''
+    firstCardElement = ''
+    return
+  }
 
-
-
+  console.log('here')
+}
