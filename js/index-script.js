8df0e5daaa1850f96154e25a78858211685cd530
maryamalihasanebrahim <maryamalihasanebrahim@gmail.com>
2025-07-07 04:33:31 +0300

    saving before renaming file

--- js/index-script.js
+++ js/index-script.js
@@ -1,19 +1,2 @@
-let levelOne = document.querySelector('#left')
-let levelTwo = document.querySelector('#middle')
-let levelThree = document.querySelector('#right')
-let header2 = document.querySelector('#ini_message')
 
-levelOne.addEventListener('click', () => {
-  levelOne.style.background = '#7E4948'
-  levelOne.style.color = '#F5C3C1'
-})
 
-levelTwo.addEventListener('click', () => {
-  levelTwo.style.background = '#7E4948'
-  levelTwo.style.color = '#F5C3C1'
-})
-
-levelThree.addEventListener('click', () => {
-  levelThree.style.background = '#7E4948'
-  levelThree.style.color = '#F5C3C1'
-})
